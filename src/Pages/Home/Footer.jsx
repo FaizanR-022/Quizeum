b252ca96786482d530b8f60409440f05c2bf624c
FaizanR-022 <faizanraza405@yahoo.com>
2024-07-18 02:33:36 +0500

    footer linkBox

--- src/Pages/Home/Footer.jsx
+++ src/Pages/Home/Footer.jsx
@@ -2,6 +2,8 @@ import {
   ButtonGroup,
   IconButton,
   Link,
+  List,
+  ListItem,
   styled,
   Typography,
 } from "@mui/material";
@@ -10,26 +12,34 @@ import FacebookIcon from "@mui/icons-material/Facebook";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
-const Wrapper = styled("div")(({ theme }) => ({
-  height: "250px",
+const href = "https://www.linkedin.com/in/faizan-raza-302360245/";
+
+const Wrapper = styled("div")(({ theme, ...props }) => ({
+  height: "220px",
   display: "flex",
   backgroundColor: theme.palette.primary.darkOrange,
-  padding: "80px 110px",
-  //   justifyContent
+  padding: "80px 100px",
+  justifyContent: "space-between",
+  color: theme.palette.primary.text,
 }));
+
+// Info Box styling
+
 const InfoBox = styled("div")(({ theme, ...props }) => ({
   width: "30%",
   display: "flex",
   flexWrap: "wrap",
-  gap: "35px",
+  gap: "30px",
+  paddingRight: "20px",
 }));
-const LinkBox = styled("div")(({ theme, ...props }) => ({ width: "30%" }));
-const ContactBox = styled("div")(({ theme, ...props }) => ({ width: "30%" }));
-const Logo = styled("img")(({ theme, ...props }) => ({}));
+const Logo = styled("img")(({ theme, ...props }) => ({
+  width: "300px",
+}));
+
 const Text = styled(Typography)(({ theme, ...props }) => ({
-  font: "300 17px Poppins",
-  color: theme.palette.primary.text,
+  font: "300 15px Poppins",
 }));
+
 const Icons = styled(ButtonGroup)(({ theme, ...props }) => ({
   "& button": {
     color: theme.palette.primary.text,
@@ -43,10 +53,32 @@ const Icons = styled(ButtonGroup)(({ theme, ...props }) => ({
     backgroundColor: theme.palette.primary.orange,
   },
 }));
-const Icon = styled(IconButton)(({ theme, ...props }) => ({}));
-const link = styled("div")(({ theme, ...props }) => ({}));
 
-const onclick = "https://www.google.com/";
+// Link Box styling
+
+const LinkBox = styled("div")(({ theme, ...props }) => ({
+  width: "30%",
+  display: "flex",
+  flexDirection: "column",
+  flexWrap: "wrap",
+  alignContent: "center",
+
+  "& li": {
+    color: theme.palette.primary.text,
+    font: "400 14px Poppins",
+    paddingLeft: "5px",
+  },
+}));
+
+const Heading = styled(Typography)(({ theme, ...props }) => ({
+  font: "700 20px Raleway",
+  color: theme.palette.primary.main,
+  paddingBottom: "15px",
+}));
+
+const ContactBox = styled("div")(({ theme, ...props }) => ({ width: "30%" }));
+
+const link = styled("div")(({ theme, ...props }) => ({}));
 
 // const Copyright;
 
@@ -54,24 +86,48 @@ export default function Footer() {
   return (
     <Wrapper>
       <InfoBox>
-        <Logo src={logo} sx={{ width: "350px" }}></Logo>
+        {/* <Link to="/">  Add link tag from react-router-dom */}
+        <Logo src={logo}></Logo>
+        {/* </Link> */}
         <Text>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi animi
           reiciendis, nostrum harum repudiandae consequatur!
         </Text>
         <Icons>
-          <Icon LinkComponent={"a"} href={"https://www.google.com/"}>
-            <FacebookIcon />
-          </Icon>
-          <Icon>
-            <InstagramIcon />
-          </Icon>
-          <Icon>
-            <LinkedInIcon />
-          </Icon>
+          <Link href={href} target="_blank">
+            <IconButton>
+              <FacebookIcon />
+            </IconButton>
+          </Link>
+          <Link href={href} target="_blank">
+            <IconButton>
+              <InstagramIcon />
+            </IconButton>
+          </Link>
+          <Link href={href} target="_blank">
+            <IconButton>
+              <LinkedInIcon />
+            </IconButton>
+          </Link>
         </Icons>
       </InfoBox>
-      <LinkBox></LinkBox>
+      <LinkBox>
+        <Heading>Quick Links</Heading>
+        <List>
+          <Link underline="none">
+            <ListItem>Home</ListItem>
+          </Link>
+          <Link underline="none">
+            <ListItem>Programs</ListItem>
+          </Link>
+          <Link underline="none">
+            <ListItem>Your Record</ListItem>
+          </Link>
+          <Link underline="none">
+            <ListItem>Success Stories</ListItem>
+          </Link>
+        </List>
+      </LinkBox>
       <ContactBox></ContactBox>
     </Wrapper>
   );
